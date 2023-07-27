fdb55b73744394f6df509527782e75e36435713f
masdefry <muhammaddefryan@gmail.com>
2023-07-27 13:39:04 +0700

    Todos

--- my-first-app/src/Pages/Todo/Todo.jsx
+++ my-first-app/src/Pages/Todo/Todo.jsx
@@ -1,36 +1,47 @@
 import { useRef, useState } from "react";
 
-import ComponentTodo from './ComponentTodo';
-
 export default function Todo(){
 
-    const [todo, setTodo] = useState([
-        {todo: 'Makan', date:'2023-07-28'},
-    ])
-
-    const inputTodo = useRef()
+    const [todos, setTodos] = useState([])
+    
+    const inputActivity = useRef()
     const inputDate = useRef()
 
     const onSubmit = () => {
-        let currentData = [...todo]
+        // const data = {
+        //     activity: inputActivity.current.value, 
+        //     date: inputDate.current.value 
+        // }
 
-        let obj = {todo: inputTodo.current.value, date: inputDate.current.value}
-        currentData.push(obj)
-        setTodo(currentData)
+        const data = [ inputActivity.current.value, inputDate.current.value ]
+
+        const currentTodos = [...todos]
+        currentTodos.push(data)
+
+        setTodos(currentTodos)
     }
 
     return(
-        <div style={{width: '300px', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center'}}>
+        <div>
+            {console.log(todos)}
+            {
+                todos.map((value, index) => {
+                    return(
+                        <div key={index}>
+                            <span>
+                                {value[0]} 
+                            </span>
+                            <span>
+                                {value[1]}
+                            </span>
+                        </div>
+                    )
+                })
+            }
 
-            
-                <ComponentTodo data={todo} />
-       
-
-
-            <input ref={inputTodo} type='text' placeholder="Enter your activity" style={{marginTop: '50px'}} />
-            <input ref={inputDate} type='date' placeholder="Enter date" />
-        
-            <button onClick={onSubmit} >
+            <input type="text" ref={inputActivity} placeholder="Enter your activity" />
+            <input type="date" ref={inputDate} placeholder="Enter date" />
+            <button onClick={onSubmit}>
                 Submit
             </button>
         </div>
